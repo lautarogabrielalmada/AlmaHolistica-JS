d702c3c4b501f81aac41ac9703c99ef3d3a56597
lautaroalmada <lautarogabrielalmada@gmail.com>
2022-09-24 16:29:38 -0300

    Se termino el ejercicio 1 coder

--- js/codigo.js
+++ js/codigo.js
@@ -3,9 +3,16 @@ let sahumerio = 500;
 let perfume = 3500;
 let cantidad;
 let precioFinal;
+let edad;
 let mensaje = "el precio final es de : "
 const iva= 0.21;
 let producto;
+
+edad = parseInt(prompt("¿Cuantos años tiene?"));
+
+while(edad < 18){
+    edad = parseInt(prompt("Solo mayores de 18 pueden comprar"));
+}
 
 producto = prompt("¿Que quiere comprar? (Sahumerio, Perfume)").toLocaleLowerCase();
 
@@ -16,15 +23,15 @@ if(producto == "sahumerio"){
     precioFinal = sahumerio * cantidad;
     
 
-    alert(`Elegiste el sahumerio ${mensaje} ${precioFinal}`);
+    alert(`Elegiste el sahumerio ${mensaje} $ ${precioFinal}`);
 
 }else if(producto == "perfume"){
 
     cantidad = prompt("Cuantos perfumes quiere?");
-    sahumerio += perfume * iva ;
+    perfume += perfume * iva ;
     precioFinal = perfume * cantidad;
 
-    alert(`Elegiste el perfume ${mensaje} ${precioFinal}`);
+    alert(`Elegiste el perfume ${mensaje} $ ${precioFinal}`);
 }
 
 
