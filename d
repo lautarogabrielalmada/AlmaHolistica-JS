0352128f420e55b4d437d2c124dbf9ecb393e2d7
lautaroalmada <lautarogabrielalmada@gmail.com>
2022-10-09 17:58:30 -0300

    Se termino la compra y agregar carrito

---
+++ d
@@ -0,0 +1,53 @@
+[33mcommit 3b1eb38406ca77cc37c14d97da9162c0dc4e6ee8[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: lautaroalmada <lautarogabrielalmada@gmail.com>
+Date:   Sun Oct 9 16:38:45 2022 -0300
+
+    Se agrego array productos con precio
+
+[33mcommit b69541408913bc244659d967eb1fa058a0ded43a[m[33m ([m[1;31morigin/main[m[33m)[m
+Author: lautaroalmada <lautarogabrielalmada@gmail.com>
+Date:   Sun Sep 25 13:43:06 2022 -0300
+
+    Se modifico js
+
+[33mcommit e07d40338847ce3f895887538ee3b37c6f5bac3f[m
+Author: lautaroalmada <lautarogabrielalmada@gmail.com>
+Date:   Sun Sep 25 13:01:55 2022 -0300
+
+    Se realizo funcion calcularPrecioProducto y calcularEdad
+
+[33mcommit d702c3c4b501f81aac41ac9703c99ef3d3a56597[m
+Author: lautaroalmada <lautarogabrielalmada@gmail.com>
+Date:   Sat Sep 24 16:29:38 2022 -0300
+
+    Se termino el ejercicio 1 coder
+
+[33mcommit fd887e710a37ac2e3b300da9acf11bafc4cc3e6f[m
+Author: lautaroalmada <lautarogabrielalmada@gmail.com>
+Date:   Sat Sep 24 16:18:18 2022 -0300
+
+    Recien ahora despues de 4 horas llegamos a algo, no me funciona while js
+
+[33mcommit 1bffb21e0650b41561f3e4d7e928cd230b59b98a[m
+Author: lautaroalmada <lautarogabrielalmada@gmail.com>
+Date:   Sat Sep 24 14:53:59 2022 -0300
+
+    Se termino la base de index todas las secciones
+
+[33mcommit 55ba2e5f435ebfef7ba8c6d535f90c419e12900d[m
+Author: lautaroalmada <lautarogabrielalmada@gmail.com>
+Date:   Sat Sep 24 13:32:23 2022 -0300
+
+    Se agrego js y nav bar
+
+[33mcommit 71b115ae851356a84a4d46d492cfaa234b544bd2[m
+Author: lautaroalmada <lautarogabrielalmada@gmail.com>
+Date:   Sat Sep 24 12:54:52 2022 -0300
+
+    Se agrego link css
+
+[33mcommit 0a8a6b59010fdbe36902e8e89208b227ad2c9091[m[33m ([m[1;31morigin/scss[m[33m, [m[1;31morigin/js[m[33m, [m[1;32mscss[m[33m, [m[1;32mjs[m[33m)[m
+Author: lautaroalmada <lautarogabrielalmada@gmail.com>
+Date:   Sat Sep 24 11:24:04 2022 -0300
+
+    Se da inicio a la web holistica
--- js/codigo.js
+++ js/codigo.js
@@ -56,7 +56,7 @@ calcularPrecioProductos();
 
 /*Array con los productos*/
 
-const Productos = [
+const productos = [
     {
         id:1,
         tipo:"sahumos",
@@ -108,8 +108,99 @@ const Productos = [
 
 ];
 
+let carrito = [];
+
+/*Preguntamos si quiere comprar un producto y validamos */
+
+let seleccion = prompt("¿Hola, desea comprar algun producto?").toLocaleLowerCase();
+
+console.log(seleccion);
+
+while(seleccion != "si" && seleccion != "no"){
+
+    alert("Porfavor ingrese si o no");
+    seleccion = prompt("¿Desea comprar algun producto?").toLocaleLowerCase();
+    console.log(seleccion);
+    
+};
+
+//En caso de que sea si o no 
+
+if(seleccion == "si"){
+
+    alert("Nuestros productos son los siguientes:");
+    let todosLosProductos = productos.map((producto) => producto.nombre + " " + "$" + producto.precio);
+    alert(todosLosProductos.join(" -- "));
+
+}else if(seleccion == "no"){
+    alert("Muchas gracias por visitarnos");
+}
 
 
 
+while(seleccion != "no"){
+
+    let producto = prompt(`Agrega un producto a tu carrito: Kit herbal - sahumerio natural - sahumerio sagrada madre - sahumerio aromanza - 7 chakras - piramides energeticas - bombita herbal`).toLocaleLowerCase();
+    let precio = 0;
+
+    if (producto == "kit herbal" || producto == "sahumerio natural" || producto == "sahumerio sagrada madre" || producto == "sahumerio aromanza" || producto == "7 chakras" || producto == "piramides energeticas" || producto == "bombita herbal"){
+
+        switch(producto){
+
+            case "kit herbal":
+                precio = 1220;
+                break;
+            
+            case "sahumerio natural":
+                precio = 352;
+                break;
+
+            case "sahumerio sagrada madre":
+                precio = 334;
+                break;
+
+            case "sahumerio aromanza":
+                precio = 420;
+                break;    
+                    
+            case "7 chakras":
+                precio = 352;
+                break;
+
+            case "piramides energeticas":
+                precio = 488;
+                break;
+
+            case "bombita herbal":
+                precio = 230;
+                break;
+
+            default:
+                break;
+
+        }
+
+        let unidades = parseInt(prompt("¿Cuantas unidades quiere llevar?"));
+
+        carrito.push({producto,unidades,precio});
+        console.log(carrito);
+
+        seleccion = prompt("¿Quiere seguir comprando?").toLocaleLowerCase();
+
+        while(seleccion === "no"){
+
+            alert("Gracias por comprar")
+
+             carrito.map((carritoFinal) => {
+                alert(`Producto: ${carritoFinal.producto} / Unidades: ${carritoFinal.unidades} / Precio: $ ${carritoFinal.precio} / total a pagar: $ ${carritoFinal.unidades * carritoFinal.precio}`);
+            })
+        break;
+        }
+
+    }
+}
+
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
+alert(`El total a pagar por todo es de: $ ${total}`);
 
 
