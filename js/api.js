42de3dfeb0d76c908e86f2641242a713ae0b8136
lautaroalmada <lautarogabrielalmada@gmail.com>
2022-11-06 20:33:08 -0300

    Se intenta integrar api

--- js/api.js
+++ js/api.js
@@ -54,10 +54,6 @@
 
 // ConsultarApi();
 
-const giveProduct = document.querySelector("#giveProduct");
-
-giveProduct.addEventListener("click",TraerProductos);
-
 function TraerProductos(){
     fetch("productos-json/productos.json")
 
@@ -65,86 +61,48 @@ function TraerProductos(){
         return respuesta.json()})
         
     .then(resultado => {
+        mostrarHTML(resultado);
         console.log(resultado);
     })
 }
 
 function MostrarHTML(datos){
-    productos.forEach((producto)=>{
 
-        //Creo un div con document.createelement
-        let content = document.createElement("div");
-        content.className = "card"
-        //Le asigno contenido con innerhtml
-        content.innerHTML = `
-        
-        <div class="row">
-            <div>
-                    <div class="card " style="width: 18rem;">
-                <img class="card-img-top" src="${producto.imag}" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">${producto.nombre}</h5>
-                    <p class="card-text">$${producto.precio}</p>
-                </div>
-                </div>
+    
+
+   datos.forEach(producto => {
+
+    const shopContent = document.querySelector("#shopContent");
+
+    let content = document.createElement("div");
+    content.className = "card"
+
+    const{nombre,precio,imag} = producto;
+
+    content.innerHTML = `
+            
+    <div class="row">
+        <div>
+                <div class="card " style="width: 18rem;">
+            <img class="card-img-top" src="${imag}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">${nombre}</h5>
+                <p class="card-text">$${precio}</p>
+            </div>
             </div>
         </div>
-        `
-        
-        ;
-    
-        shopContent.append(content);
-    
-        let comprar = document.createElement("button");
-        comprar.innerText = "comprar";
-        comprar.className = "comprar"
-    
-        content.append(comprar);
-    
-        
-    
-        comprar.addEventListener("click",()=>{
-    
-            const repeat = carrito.some((repeatProduct) => repeatProduct.id === producto.id);
+    </div>
             
-            if (repeat === true){
-                carrito.map((prod) => {
-                    if (prod.id === producto.id){
-                        prod.cantidad++;
-                    }
-                });
-            }else{
-                carrito.push({
-                    id: producto.id,
-                    imag:producto.imag,
-                    nombre:producto.nombre,
-                    precio:producto.precio,
-                    cantidad:producto.cantidad,
-                })
-                
-            }
-    
-            Swal.fire({
-                position: 'center',
-                icon: 'success',
-                title: 'Se agrego al carrito',
-                showConfirmButton: false,
-                timer: 1500
-              })
-    
-            carritoCounter();
-            saveLocal();
             
-        });
-    })
+            
+          `;
     
-    ///////////////////////////////////////////////////
-    
-    
-    //Stringyfi para que pase como string, solo recibe string SET
-    
-    const saveLocal = () => {
-        localStorage.setItem("carrito",JSON.stringify(carrito));
-    }
+    shopContent.append(content);
+
+
+
+   });
 };
 
+
+MostrarHTML();
