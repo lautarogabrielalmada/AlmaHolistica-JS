89b805ade64fdf2ac17c8af1a0c3e1397124b795
lautaroalmada <lautarogabrielalmada@gmail.com>
2022-11-06 21:02:55 -0300

    Se retoco el codigo index

--- js/api.js
+++ js/api.js
@@ -1,108 +1,55 @@
-// function ConsultarApi(){
+ function ConsultarApi(){
 
-//     const url = "https://picsum.photos/v2/list";
+    const url = "https://jsonplaceholder.typicode.com/comments";
 
-//     fetch(url)
+    fetch(url)
 
-//     .then( respuesta => {
-//         return respuesta.json()
-//     }) 
-//     .then(resultado => {
-//         mostrarHTML(resultado);
-//         console.log(resultado)
-//     })
+    .then( respuesta => {
+        return respuesta.json()
+    }) 
+     .then(resultado => {
+        mostrarHTML(resultado);
+         console.log(resultado)
+    })
+}; 
 
-// }; 
 
-// function mostrarHTML(datos){
 
-//     const apiContent = document.querySelector("#apiContent");
+ function mostrarHTML(datos){
 
-//     let html = "";
+    const apiContent = document.querySelector("#apiContent");
 
-//     datos.forEach(perfil => {
-        
-//         const{author,download_url} = perfil;
+    let html = "";
 
-//         html += `
-        
-//         <div class="row">
-//         <div>
-//                 <div class="card " style="width: 18rem;">
-//             <img class="card-img-top" src="${download_url}" alt="Card image cap">
-//             <div class="card-body">
-//                 <h5 class="card-title">${author}</h5>
-//                 <p></p>
-//                 <button>Ver</button>
-//             </div>
-//             </div>
-//         </div>
-//     </div>
+    datos.forEach(perfil => {
+                 const{name,email,body} = perfil;
+
+        html += `     
+        <div class="comment mt-4 text-justify float-left">
+                    <img src="https://i.imgur.com/yTFUilP.jpg" alt="" class="rounded-circle" width="40" height="40">
+                    <h4>${name}</h4>
+                    <span>${email}</span>
+                    <br>
+                    <p>${body}</p>
+                </div>
         
         
         
-//         `;
+        `;
 
-//         apiContent.innerHTML = html;
+         apiContent.innerHTML = html;
 
 
-//     });
+     });
 
-// };
+};
+
 
 
 
 // ConsultarApi();
 
-function TraerProductos(){
-    fetch("productos-json/productos.json")
-
-    .then(respuesta => {
-        return respuesta.json()})
-        
-    .then(resultado => {
-        mostrarHTML(resultado);
-        console.log(resultado);
-    })
-}
-
-function MostrarHTML(datos){
-
-    
-
-   datos.forEach(producto => {
-
-    const shopContent = document.querySelector("#shopContent");
-
-    let content = document.createElement("div");
-    content.className = "card"
-
-    const{nombre,precio,imag} = producto;
-
-    content.innerHTML = `
-            
-    <div class="row">
-        <div>
-                <div class="card " style="width: 18rem;">
-            <img class="card-img-top" src="${imag}" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">${nombre}</h5>
-                <p class="card-text">$${precio}</p>
-            </div>
-            </div>
-        </div>
-    </div>
-            
-            
-            
-          `;
-    
-    shopContent.append(content);
 
 
 
-   });
-};
 
-
-MostrarHTML();
--- js/carrito.js
+++ js/carrito.js
@@ -98,6 +98,7 @@ const eliminarProducto = (id) => {
         return carritoId !== foundId;
     });
 
+
     carritoCounter();
     saveLocal();
     pintarCarrito();
